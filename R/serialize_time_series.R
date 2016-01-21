eec78ac140d1edf78f708d6f6648022a2b21ee85
Carl Schroedl <cschroedl@usgs.gov>
2016-01-21 17:22:01 -0600

    Adding serialization check into regular R package build.

--- R/serialize_time_series.R
+++ R/serialize_time_series.R
@@ -1,20 +1,42 @@
-BASE_PATH <- 'inst/extdata/'
 EXTENSION <- '.csv'
 write_out <- function(dataframe_name){
 	dataframe <- get(dataframe_name)
-	file_name <- paste(BASE_PATH, dataframe_name, EXTENSION, sep = "")
+	file_name <- paste(get_serialized_data_dir(), dataframe_name, EXTENSION, sep = "")
 	write.csv(dataframe, file_name, quote = TRUE, na = "NULL")
 	return(file_name);
 }
 
+#' @export
+get_serialized_data_dir <- function(){
+	dir_sep <- '/'
+	if (.Platform$OS.type == 'windows') {
+		dir_sep <- '\\'
+	}
+	serialized_data_dir <- paste('inst', 'extdata', sep = dir_sep)
+	#append a trailing slash
+	serialized_data_dir <- paste(serialized_data_dir, dir_sep, sep = '')
+	
+	return(serialized_data_dir)
+}
+
+#' @export
 serialize_time_series <- function(){
-	data_frame_names <- c(
-		'discqw',
-		'aloads',
-		'mloads',
-		'aflow',
-		'mflow',
-		'dflow'
-	)
-	lapply(data_frame_names, write_out)
+	serialized_data_dir <- get_serialized_data_dir()
+	if (!dir.exists(serialized_data_dir)) {
+		stop(paste(
+			'The function "serialize_time_series" must be run with the git',
+			'repository as your working directory. Current working directory is:',
+			getwd())
+		 )
+	} else {
+		data_frame_names <- c(
+			'discqw',
+			'aloads',
+			'mloads',
+			'aflow',
+			'mflow',
+			'dflow'
+		)
+		return(lapply(data_frame_names, write_out))
+	}
 }
---
+++ tests/test_serialization.R
@@ -0,0 +1,36 @@
+library(testthat)
+library(nardata)
+context("serialization")
+
+test_that("serialization is current", {
+	
+	old_wd <- getwd()
+	
+	tryCatch({
+		#linux or OSX
+		dir_sep <- '/'
+		if (.Platform$OS.type == 'windows') {
+			dir_sep <- '\\'
+		}
+		relative_path_to_repo_root <- paste(rep('..', 2), collapse = dir_sep)
+		relative_path_to_repo_root <- paste(relative_path_to_repo_root, 'nar_data', sep = dir_sep)
+		
+		setwd(relative_path_to_repo_root)
+		nardata::serialize_time_series()
+		path_to_data_dir_from_repo_root <- nardata::get_serialized_data_dir()
+		
+		git_command <- paste('git ls-files -mo', path_to_data_dir_from_repo_root)
+		
+		stale_or_new_files <- system( git_command, intern = TRUE)
+		
+		if (0 != length(stale_or_new_files)) {
+			stale_or_new_files <- paste(stale_or_new_files, collapse = ', ')
+			warning(stale_or_new_files)
+			testthat::fail(paste("The following serialized files are not current. Try running nardata::serialize_time_series(), and adding and committing the resulting files.:", stale_or_new_files))
+		} else {
+			testthat::succeed("Serialized files are not checked")
+		}
+	}, finally = {
+		setwd(old_wd)
+	})
+})
