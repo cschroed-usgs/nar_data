f1b5f56449355b00235961a70ceecceed5057553
Carl Schroedl <cschroedl@usgs.gov>
2016-01-22 11:18:35 -0600

    adding debug prints for CI.

--- tests/test_serialization.R
+++ tests/test_serialization.R
@@ -5,7 +5,7 @@ context("serialization")
 test_that("serialization is current", {
 	
 	old_wd <- getwd()
-	
+	warning(' initial working directory: ', old_wd)
 	tryCatch({
 		#linux or OSX
 		dir_sep <- '/'
@@ -14,18 +14,18 @@ test_that("serialization is current", {
 		}
 		relative_path_to_repo_root <- paste(rep('..', 2), collapse = dir_sep)
 		relative_path_to_repo_root <- paste(relative_path_to_repo_root, 'nar_data', sep = dir_sep)
-		
+		warning('going to attempt to set wd to : ', relative_path_to_repo_root)
 		setwd(relative_path_to_repo_root)
 		nardata::serialize_time_series()
 		path_to_data_dir_from_repo_root <- nardata::get_serialized_data_dir()
-		
+		warning('path_to_data_dir_from_repo_root: ', path_path_to_data_dir_from_repo_root)
 		git_command <- paste('git ls-files -mo', path_to_data_dir_from_repo_root)
-		
+		warning('git command: ', git_command)
 		stale_or_new_files <- system( git_command, intern = TRUE)
 		
 		if (0 != length(stale_or_new_files)) {
 			stale_or_new_files <- paste(stale_or_new_files, collapse = ', ')
-			warning(stale_or_new_files)
+			warning('stale or new files: ', stale_or_new_files)
 			testthat::fail(paste("The following serialized files are not current. Try running nardata::serialize_time_series(), and adding and committing the resulting files.:", stale_or_new_files))
 		} else {
 			testthat::succeed("Serialized files are not checked")
